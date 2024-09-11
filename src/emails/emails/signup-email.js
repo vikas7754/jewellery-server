b5bf38ec216f5109404d211a3dc60e143ecd8974
Vikas <vikask4590@gmail.com>
2024-09-11 20:32:09 +0530

    fixed signup route

--- src/emails/emails/signup-email.js
+++ src/emails/emails/signup-email.js
@@ -2,7 +2,7 @@ const sendEmail = require("../send-email");
 const signupMail = async (data) => {
   try {
     const subject = `Welcome to our platform ${data.name}`;
-    await sendEmail(data.email, subject, data.toObject(), "signup.hbs");
+    await sendEmail(data.email, subject, data, "signup.hbs");
   } catch (err) {
     throw err;
   }

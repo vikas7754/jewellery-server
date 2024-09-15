0f91b97d134666e4a7599e64d51762e94e4a2a9b
Vikas <vikask4590@gmail.com>
2024-09-15 19:55:27 +0530

    fixed

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const mongoose = require("mongoose");
 const cookieParser = require("cookie-parser");
 
 const app = express();
-const port = 8000;
+const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(bodyParser.json());
